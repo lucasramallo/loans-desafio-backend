35c26194d80d75ef337f6caadb2696ded4c37ad5
lucasramallo <lucasdebrito.jd@gmail.com>
2024-04-11 20:50:15 -0300

    initial commit

---
+++ src/main/java/com/github/lucasramallo/loans/LoansApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasramallo.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
